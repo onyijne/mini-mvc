978b5d662865919912113ca92c8a8b5117fe89a8
Samuel Onyijne <samuelord1@gmail.com>
2019-08-10 02:26:03 +0100

    initial commit

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 $base = dirname(__DIR__);
-defined('HOMEURL') or define('HOMEURL', '');/* /sub/folder (without an ending forward slash)*/
+defined('HOMEURL') or define('HOMEURL', '');/* /sub/folder (without an ending forward slash) when not in root*/
 include $base.'/Autoload.php';
 include $base.'/config/main.php';
 $app = new \mini\core\Sam($config);

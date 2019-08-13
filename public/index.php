1e90a485f2f26d0430a6f8a24454b7da32f7f08c
Samuel Onyijne <samuelord1@gmail.com>
2019-08-13 01:07:13 +0100

    index.php commented

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 $base = dirname(__DIR__);
 defined('SAM_DEV') or define('SAM_DEV', 'dev');//you can comment out in production
-defined('HOMEURL') or define('HOMEURL', '');/* /sub/folder (without an ending forward slash) when not in root*/
+/* 
+* /sub/folder (without an ending forward slash) when not in root. 
+* in production it is recommended you put other file under your web-server un-accessible directory.
+*/
+defined('HOMEURL') or define('HOMEURL', '/public');
 include $base.'/Autoload.php';
 include $base.'/config/main.php';
 $app = new \mini\core\Sam($config);
--- public/php-info.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/* 
- * Check LICENSE file for license terms
- * (c) 2019. Samuel Onyijne, <samuel@sajflow.com>  * 
- */
-phpinfo();
